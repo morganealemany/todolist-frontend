8c09c69dc2512918c6cf52d5d02b7c541aa033a1
morganealemany <m.plancheron@gmail.com>
2021-07-21 16:17:10 +0200

    #4 Ajout d'une nouvelle tache dans la liste

--- assets/js/components/newTaskForm.js
+++ assets/js/components/newTaskForm.js
@@ -13,7 +13,7 @@
         // ----------------------------------------------------------------
         // On récupère l'élément du DOM correspondant au formulaire
         const newTaskFormElement = document.querySelector('form');
-        console.log(newTaskFormElement);
+        
         // Dès qu'on clique sur le titre de la tâche, on passe en mode édition
         newTaskFormElement.addEventListener('submit', newTaskForm.handleNewTaskFormSubmit);
     },
@@ -24,8 +24,32 @@
      * @param {Event} evt 
      */
     handleNewTaskFormSubmit: function(evt) {
+        // On bloque le comportement par défaut (le rechargement de la page) du formulaire
         evt.preventDefault();
-        console.log('Soumission du formulaire');
+
+        // On récupére l'élément sur lequel l'événement a eu lieu
+        const newTaskFormElement = evt.currentTarget;
+
+        // On récupére la valeur de l'input pour le nom de la tâche
+        const newTaskTitle = newTaskFormElement.querySelector('input[name="title"]').value;
+
+        // On récupére la valeur de l'input select pour le nom de la catégorie
+        const newTaskCategory = newTaskFormElement.querySelector('select').value;
+
+        // On se sert de la balise <template> qui contient un morceau de code complet (avec balises etc) et on la clone afin d'ajouter une nouvelle tâche 
+        let newTaskElement = document.getElementById('empty-task').content.cloneNode(true);
+
+        // newTaskElement est un clone 'vide', ajoutons les infos dans ses enfants
+        newTaskElement.querySelector('.task__title-label').textContent = newTaskTitle;
+        newTaskElement.querySelector('.task__category > p').textContent = newTaskCategory;
+
+        // il ne reste plus qu'à ajouter ce template à notre liste
+        // pour cela on cible l'élément parent à toutes les tâches
+        const tasksListElement = document.querySelector('.tasks');
+
+        // puis on ajoute la nouvelle tâche comme enfant de l'élément parent à toutes les tâches
+        tasksListElement.appendChild(newTaskElement);
+    
     },
  
 }
