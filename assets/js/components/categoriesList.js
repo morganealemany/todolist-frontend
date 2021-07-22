8541d12dff5f00e4185fec8bfdce4fbf913b98f7
morganealemany <m.plancheron@gmail.com>
2021-07-22 14:37:52 +0200

    #2 requete HTTP Via AJAX pour la liste des catégories

--- assets/js/components/categoriesList.js
+++ assets/js/components/categoriesList.js
@@ -12,5 +12,32 @@
 
     loadCategoriesFromAPI: function() {
         console.log('loadCategoriesFromAPI');
+
+        let myList = document.querySelector('ul');
+
+        // On prépare la configuration de la requête HTTP
+        let config = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache'
+        };
+
+        // On déclenche la requête HTTP (via le moteur sous-jacent Ajax)
+        fetch('https://benoclock.github.io/S07-todolist/categories.json', config)
+        // Ensuite, lorsqu'on reçoit la réponse au format JSON
+        .then(function(response) {
+            // On convertit cette réponse en un objet JS et on le retourne
+            console.log(response, response.json);
+            return response.json();
+        })
+        // Ce résultat au format JS est récupéré en argument ici-même
+        .then(function(data) {
+            console.log(data);
+            // On dispose désormais d'un tableau JS exploitable dans la variable data
+            // La suite dépend de l'utilisation qu'on veut faire de ces données
+            
+            }
+        );
+
     },
 }
